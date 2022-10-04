19a7fe2393346a56fd187a5c9201c3397401e56f
David Salmberg <salmberg@hotmail.com>
2022-10-04 21:11:58 +0200

    80% klart

---
+++ src/Board.java
@@ -0,0 +1,119 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Board {
+
+    public ArrayList<Integer> p1Pos = new ArrayList<>();
+    public ArrayList<Integer> p2Pos = new ArrayList<>();
+
+    public char[][] playingBoard = fillPlayingBoard();
+
+    public  char[][] fillPlayingBoard() {
+        return new char[][]{{' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}};
+    }
+
+
+    public static void printOutPlayingBoard(char[][] playingBoard) { //This for loop prints out the playingboard
+        for (char[] row : playingBoard) {
+            for (char col : row) {
+                System.out.print(col);
+
+            }
+            System.out.println();
+        }
+    }
+
+    public void placeYourDraw(int pos, String user) { //This is the method that lets the player place their "X" or "O".
+
+        char symbol = ' ';
+        char getCurrentPositionChar = getPosition(pos);
+
+        if (user.equals("player1")) {
+            symbol = 'X';
+        } else if (user.equals("player2")) {
+            symbol = 'O';
+        }
+            if (Character.toString(getCurrentPositionChar).equals(" ")) { //Om den är tom går det att placera in X/O
+
+                //Lägger till X/O
+                if (user.equals("player1")) {
+                    p1Pos.add(pos);
+                } else if (user.equals("player2")) {
+                    p2Pos.add(pos);
+                }
+
+                //Placerar in X/O på respektive plats
+                switch (pos) {
+                    case 1 -> playingBoard[0][0] = symbol;
+                    case 2 -> playingBoard[0][2] = symbol;
+                    case 3 -> playingBoard[0][4] = symbol;
+                    case 4 -> playingBoard[2][0] = symbol;
+                    case 5 -> playingBoard[2][2] = symbol;
+                    case 6 -> playingBoard[2][4] = symbol;
+                    case 7 -> playingBoard[4][0] = symbol;
+                    case 8 -> playingBoard[4][2] = symbol;
+                    case 9 -> playingBoard[4][4] = symbol;
+                    default -> {
+                    }
+                }
+            } else {
+                System.out.println("Already taken, try again!:");
+            }
+        }
+
+
+    public String checkWinner(){
+        List firstRow = Arrays.asList(1, 2, 3);
+        List secondRow = Arrays.asList(4, 5, 6);
+        List thirdRow = Arrays.asList(7, 8, 9);
+        List firstCol = Arrays.asList(1, 4, 7);
+        List secondCol = Arrays.asList(2, 5, 8);
+        List thirdCol = Arrays.asList(3, 6, 9);
+        List cross1 = Arrays.asList(1, 5, 9);
+        List cross2 = Arrays.asList(3, 5, 7);
+
+        List<List> winning = new ArrayList<List>();
+        winning.add(firstRow);
+        winning.add(secondRow);
+        winning.add(thirdRow);
+        winning.add(firstCol);
+        winning.add(secondCol);
+        winning.add(thirdCol);
+        winning.add(cross1);
+        winning.add(cross2);
+
+//        System.out.println("WiningList:: " + winning);
+        // System.out.println("Play 1 positions:: " + p1Pos);
+        // System.out.println("Play 2 positions:: " + p2Pos);
+
+
+        for (List l : winning) {
+//            System.out.println("List:: " + winning);
+
+            if (p1Pos.containsAll(l)) {
+                return " you  won!";
+            } else if (p2Pos.containsAll(l)) {
+                return " you won!";
+            }
+             if (p1Pos.size() + p2Pos.size() == 9) {
+                return "Its a tie!";
+             }
+        }
+        return "";
+    }
+    public void clearBoard(){
+        p1Pos.clear();
+        p2Pos.clear();
+        playingBoard = fillPlayingBoard();
+
+    }
+}
+
+
+
+
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,27 +1,98 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TicTacToe {
+
+    static Board board = new Board();
+
     public static void main(String[] args) {
 
-        System.out.println("Welcome to this Tic Tac Toe Game!");
+        boolean whileRunning = true;
+        while (whileRunning) {
+
+            System.out.println("Welcome to this Tic Tac Toe Game!");
+            System.out.println("<===============================>");
+            System.out.println("The rules are simple:");
+            System.out.println("1.The game is played on a grid that's 3 squares by 3 squares.");
+            System.out.println("2.Player one is X, player two is is O. Players take turns putting their marks in empty squares (1-9)");
+            System.out.println("3.The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner.");
+            System.out.println("4.When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie.");
+            System.out.println("<===============================>");
 
 
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Player 1 (X), enter your name: ");
+            String p1 = sc.nextLine();
+            System.out.println("Player 2 (O), enter your name: ");
+            String p2 = sc.nextLine();
 
-        char[][] playingBoard = {{' ', '|', ' ', '|', ' '}, //This is the playingboard
-                {'-', '+', '-', '+', '-'},
-                {' ', '|', ' ', '|', ' '},
-                {'-', '+', '-', '+', '-'},
-                {' ', '|', ' ', '|', ' '}};
 
-        printPlayingBoard.printOutPlayingBoard(playingBoard); //This one is calling in the method "PrintOutPlayingBoard"
+            board.printOutPlayingBoard(); //This one is calling in the method "PrintOutPlayingBoard"
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Make your draw (1-9)");
-        int pos = sc.nextInt();
 
-        placeDraw.placeYourDraw(playingBoard, pos, "player");
+            boolean exitWhileLoop = false; //Denna bool är false by default
 
-        printPlayingBoard.printOutPlayingBoard(playingBoard);
+            while (!exitWhileLoop) {
 
+                //System.out.print(player + " make your draw (1-9):");
+                //Här vill jag ha en while loop som kör så länge pos är invalid och positionen är upptagen.
+                int pos1 = getValidPosition(sc, p1); //kallar på metoden getValidPosition
+
+
+                System.out.println(pos1);
+
+                board.placeYourDraw(pos1, "player1");// Prints out the playingboard + the chosen position of "X".
+                board.printOutPlayingBoard();
+
+                String result = board.checkWinner();
+
+
+                if (result.length() > 0) { //Om detta är sant så sätts boolen till true och då ska while-loopen sluta
+                    System.out.println(p1 + result);
+                    break;
+                }
+
+                //----------------------------------------------------//
+
+
+                int pos2 = getValidPosition(sc, p2);
+
+                System.out.println(pos2);
+
+                board.placeYourDraw(pos2, "player2"); //Prints out the playingboard + the position of "O".
+                board.printOutPlayingBoard();
+
+                result = board.checkWinner();
+
+
+                if (result.length() > 0) {
+                    System.out.println(p2 + result);
+                    break;
+                }
+            }
+            board.clearBoard();
+            System.out.println("Lets play again!");
+            sc.nextLine();
+        }
+    }
+
+    private static int getValidPosition(Scanner sc, String player) { //Denna metod tar in en scanner och kollar om player har skrivit in rätt symbol (1-9)
+        boolean whileInvalid = true;
+       int pos = 0;
+
+        System.out.print(player + " make your draw (1-9):");
+        while (whileInvalid) {
+            String pos1candidate = sc.nextLine(); //Input of player 1 move
+
+            try {
+                pos = Integer.parseInt(pos1candidate);
+                break;
+
+            } catch (NumberFormatException e) {
+
+                System.out.println("Invalid Symbol, try again! (1-9):");
+            }
+        }
+        return pos;
     }
 }
